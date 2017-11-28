b3bdaa1fd2c7a6682221f61adac638bee18d1ba5
Unknown <1cheaterok1@gmail.com>
2017-11-28 14:13:50 +0300

    Добавляет четвёртое задание

---
+++ Задание 4/1.hs
@@ -0,0 +1,25 @@
+import Control.Applicative (Applicative(..))
+import Control.Monad       (liftM, ap)
+
+data FunMonad a = FunMonad{ fun :: () -> a }
+
+instance (Show fun) => Show (FunMonad fun) where
+  show (FunMonad fun) = show "FunMonad " ++ (show $ fun ())
+
+-- https://wiki.haskell.org/Functor-Applicative-Monad_Proposal#Missing_superclasses
+instance Functor FunMonad where
+    fmap = liftM
+
+instance Applicative FunMonad where
+    pure x = FunMonad (\() -> x)
+    (<*>) = ap
+
+instance Monad FunMonad where
+    return = pure
+    m >>= k = k $ fun m ()
+
+
+-- "FunMonad "[5,5,5]
+test = do
+  fm <- FunMonad (\() -> 5)
+  return fm >>= (\v -> FunMonad $ \() -> [v, v, v])
